3cf66727429a430e0b9de9ad8fda6f8e73d02468
hutingjin <179645132@qq.com>
2018-10-16 11:35:00 +0800

    'second'

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <home-header></home-header>
+</template>
+
+<script>
+import HomeHeader from '@/pages/home/components/HomeHeader'
+
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
---
+++ src/pages/home/components/HomeHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <span class="iconfont">&#xe7ea;</span>
+      </div>
+      <div>
+        <span class="iconfont">&#xe632;</span>
+        <span>输入城市/景点/游玩主题</span>
+      </div>
+      <span>三亚</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeHeader'
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
